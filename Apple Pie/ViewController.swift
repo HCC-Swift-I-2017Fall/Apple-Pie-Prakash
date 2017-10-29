5a16009c919814394833c0d8045d382e8e82953f
psachania <prakash.sachania@in.ibm.com@prakashs-macbook-pro15.local>
2017-10-29 17:44:19 -0500

    First version

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -10,16 +10,97 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    var listOfWords = ["program", "swift"] //, "glorious", "incandescent", "bug", "buccaneer"]
+    let incorrectMovesAllowed = 7
+    
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var currentGame: Game!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        newRound()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func newRound() {
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord,
+                               incorrectMovesRemaining: incorrectMovesAllowed,
+                               guessedLetters: [])
+            enableLetterButtons(true)
+            updateUI()
+        } else {
+            enableLetterButtons(false)
+            correctWordLabel.text = "All rounds completed"
+        }
     }
+    
+    func enableLetterButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+            if (enable) {
+                button.layer.backgroundColor = UIColor.cyan.cgColor
+            } else {
+                button.layer.backgroundColor = UIColor.lightGray.cgColor
+            }
+        }
+    }
+    
+    func updateUI() {
+//        correctWordLabel.text = currentGame.formattedWord
+        
+        var letters = [String]()
+        for letter in currentGame.formattedWord.characters {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
+        
+        scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
+        treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
+    }
+    
+    @IBAction func letterTapped(_ sender: UIButton) {
+        sender.isEnabled = false
+        sender.layer.backgroundColor = UIColor.lightGray.cgColor
 
+        let letterString = sender.title(for: .normal)!
+        let letter = Character(letterString.lowercased())
+        currentGame.playerGuessed(letter: letter)
+        updateGameState()
+    }
+    
+    @IBAction func restartTapped(_ sender: UIButton) {
+        listOfWords = ["glorious", "incandescent", "bug", "buccaneer"]
+        newRound()
+    }
+    
+    func updateGameState() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
 
+    }
 }
-
